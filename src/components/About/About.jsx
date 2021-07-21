003f2826558d7ff59c6c05615d04fb07aae506b2
Georges FORTRY <gfortry@hotmail.com>
2021-07-22 00:28:39 +0200

    about ok

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,61 @@
+import { Link } from 'react-router-dom';
 import SAbout from './Style';
 
 export default function About() {
-  return <SAbout>About</SAbout>;
+  return (
+    <SAbout>
+      <div className="career">
+        <img className="imgcoding" src="/img/coding-man.jpg" alt="coding-man" />
+        <div className="textcareer">
+          <h1>Mon parcours</h1>
+          <p>
+            Après 17 ans d’expérience dans le domaine juridique, j’ai choisi de
+            me reconvertir dans le développement web.
+          </p>
+          <p>
+            {' '}
+            La formation au sein de la Wild Code School m’a permis d’acquérir
+            toutes les connaissances techniques et de les allier à mes qualités
+            que sont le sens de l’observation, l’écoute, la rigueur ou
+            l’autonomie.
+          </p>
+          <p>
+            {' '}
+            Je souhaite désormais m’investir dans une entreprise dynamique et
+            innovante, en apportant mes compétences, ma détermination et ma
+            polyvalence.
+          </p>
+          <Link className="btn-career" to="/career">
+            En savoir plus...
+          </Link>
+        </div>
+      </div>
+      <div className="project">
+        <div className="textproject">
+          <h2>Mes réalisations</h2>
+          <p>
+            Dans le cadre de ma formation à la Wild Code School de Lille, j’ai
+            pu réaliser de nombreux projets de groupe ainsi que des travaux en
+            autonomie.
+          </p>
+          <p>
+            {' '}
+            J’ai, ainsi, participé à la création d’un site de e-commerce (HTML,
+            CSS, JS Vanilla), à la création d’un quiz de personnalité sur le
+            thème des super-héros (ReactJS) ainsi qu’à la création d’un site
+            pour l’association Edtech Hauts-de-France (ReactJS, NodeJS){' '}
+          </p>
+          <p>
+            {' '}
+            Lors de deux hackathons, j’ai contribué à la création d’une réplique
+            du site Netflix ainsi qu’un projet en partenariat avec Fiverr.
+          </p>
+          <Link className="btn-project" to="/project">
+            En savoir plus...
+          </Link>
+        </div>
+        <img className="imgproject" src="/img/project.jpg" alt="project" />
+      </div>
+    </SAbout>
+  );
 }
--- src/components/About/Style.jsx
+++ src/components/About/Style.jsx
@@ -1,7 +1,126 @@
 import styled from 'styled-components';
 
 const SAbout = styled.section`
+  height: 160vh;
   color: #2c2875;
+  margin-top: 120px;
+  .career {
+    display: flex;
+    padding-left: 350px;
+    padding-right: 350px;
+    position: relative;
+    .textcareer {
+      position: relative;
+      background-color: #fff;
+      padding: 3em 4em 6em;
+      box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+      z-index: 1;
+      width: 400px;
+      min-height: 400px;
+      max-height: 400px;
+      margin-top: 40px;
+      margin-left: 650px;
+      border-radius: 20% 80% 30% 70%/70% 30% 70% 30%;
+      h1 {
+        padding-top: 1.8em;
+        padding-bottom: 0.8em;
+        font-size: 1.9em;
+        font-weight: bolder;
+      }
+      p {
+        font-size: 1em;
+        line-height: 1.5em;
+        span {
+          font-weight: bold;
+        }
+      }
+      .btn-career {
+        background: linear-gradient(#61dafb, #2c2875);
+        border: none;
+        text-align: center;
+        border-radius: 25px;
+        padding: 10px 5px;
+        display: block;
+        margin-top: 40px;
+        margin-left: 150px;
+        width: 150px;
+        color: #ffffff;
+        text-decoration: none;
+        box-shadow: 0 0 1px transparent;
+      }
+      .btn-career:hover {
+        width: 155px;
+        padding: 15px 10px;
+      }
+    }
+
+    .imgcoding {
+      position: absolute;
+      height: 600px;
+      width: 800px;
+      object-fit: cover;
+    }
+  }
+
+  .project {
+    display: flex;
+    padding-left: 350px;
+    padding-right: 350px;
+    padding-top: 150px;
+    position: relative;
+    .textproject {
+      position: relative;
+      background-color: #fff;
+      padding: 3em 4em 6em;
+      box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+      z-index: 1;
+      width: 400px;
+      min-height: 400px;
+      max-height: 400px;
+      margin-top: 40px;
+      border-radius: 20% 80% 30% 70%/70% 30% 70% 30%;
+
+      h2 {
+        padding-top: 1.5em;
+        padding-bottom: 0.8em;
+        font-size: 1.9em;
+        font-weight: bolder;
+      }
+      p {
+        font-size: 1em;
+        line-height: 1.5em;
+        span {
+          font-weight: bold;
+        }
+      }
+      .btn-project {
+        background: linear-gradient(#61dafb, #2c2875);
+        border: none;
+        text-align: center;
+        border-radius: 25px;
+        padding: 10px 5px;
+        display: block;
+        margin-top: 30px;
+        margin-left: 150px;
+        width: 150px;
+        color: #ffffff;
+        text-decoration: none;
+        box-shadow: 0 0 1px transparent;
+      }
+      .btn-project:hover {
+        width: 155px;
+        padding: 15px 10px;
+      }
+    }
+
+    .imgproject {
+      position: absolute;
+      right: 350px;
+      height: 600px;
+      width: 800px;
+      object-fit: cover;
+    }
+  }
 `;
 
 export default SAbout;
