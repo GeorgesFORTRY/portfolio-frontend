7f5ff7cfe514ac91821411f620cc2fa121284376
Georges FORTRY <gfortry@hotmail.com>
2021-07-21 23:25:10 +0200

    slider ok

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,81 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import SSlider from './Style';
 
+dotenv.config();
+
 export default function Slider() {
-  return <SSlider>Slider</SSlider>;
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/skills`).then(({ data }) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <SSlider>
+      <h1>Mes compétences</h1>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlay
+        autoPlaySpeed={4000}
+        centerMode={false}
+        className="Carousel"
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 4,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 2,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 3,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={4}
+        swipeable
+      >
+        {skills.map((skill) => {
+          return (
+            <div key={skill.id} className="slide">
+              <img src={skill.logoSrc} alt={skill.logoAlt} />
+            </div>
+          );
+        })}
+      </Carousel>
+    </SSlider>
+  );
 }
--- src/components/Slider/Style.jsx
+++ src/components/Slider/Style.jsx
@@ -2,6 +2,47 @@ import styled from 'styled-components';
 
 const SSlider = styled.section`
   color: #2c2875;
+  h1 {
+    text-align: center;
+    font-size: 2.8rem;
+    font-weight: bold;
+    margin-bottom: 50px;
+    @media (max-width: 400px) {
+      font-size: 2rem;
+    }
+  }
+  .Carousel {
+    margin: auto;
+    height: 20vh;
+    .react-multiple-carousel__arrow--left {
+      left: 10px;
+    }
+    .react-multiple-carousel__arrow--right {
+      right: 10px;
+    }
+    .slide {
+      display: flex;
+      align-items: center;
+      overflow: hidden;
+      margin: auto;
+      width: 10vw;
+      height: 15vh;
+      @media (max-width: 600px) {
+        width: 30vw;
+        height: 35vh;
+      }
+      img {
+        display: block;
+        max-height: 100%;
+        max-width: 100%;
+        filter: grayscale(100%);
+        margin: auto;
+      }
+      img:hover {
+        filter: grayscale(0%);
+      }
+    }
+  }
 `;
 
 export default SSlider;
