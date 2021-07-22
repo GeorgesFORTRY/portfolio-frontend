ae6678301e20a0bcb6a0a1a5a046fb4d05dd7a84
Georges FORTRY <gfortry@hotmail.com>
2021-07-22 15:39:55 +0200

    career page ok

--- src/components/Career/Career.jsx
+++ src/components/Career/Career.jsx
@@ -1,5 +1,107 @@
 import SCareer from './Style';
 
 export default function Career() {
-  return <SCareer>Career</SCareer>;
+  return (
+    <SCareer>
+      <h5>A PROPOS</h5>
+      <div className="aboutme">
+        <div className="image">
+          <img src="/img/me.jpg" alt="" />
+        </div>
+        <div className="description">
+          <div className="profil">
+            <p>
+              Après 17 ans d’expérience dans le domaine juridique, j’ai choisi
+              de me reconvertir dans le développement web.
+            </p>
+            <p>
+              {' '}
+              La formation au sein de la Wild Code School m’a permis d’acquérir
+              toutes les connaissances techniques et de les allier à mes
+              qualités.
+            </p>
+            <p>
+              {' '}
+              Je souhaite désormais m’investir dans une entreprise dynamique et
+              innovante, en apportant mes compétences, ma détermination et ma
+              polyvalence.
+            </p>
+          </div>
+          <div className="softskills">
+            <div className="qualities">
+              <h1>Mes qualités</h1>
+              <p className="ecoute">Ecoute</p>
+              <p className="rigueur">Rigueur</p>
+              <p className="autonomie">Autonomie</p>
+              <p className="fiabilite">Fiabilité</p>
+              <p className="reflechi">Réfléchi</p>
+              <p className="esprit">Esprit d’équipe</p>
+              <p className="observateur">Observateur</p>
+              <p className="analyse">Analyse</p>
+            </div>
+            <div className="interest">
+              <h2>Mes centres d’intérêt</h2>
+              <p>
+                <span>Sport</span> Course à pied (Semi-marathons, Trails,
+                Marathons), VTT, ancien arbitre de football
+              </p>
+              <p>
+                <span>Cinéma</span>
+              </p>
+              <p>
+                <span>Voyages</span> (New-York, Thaïlande, Tanzanie, Afrique du
+                Nord, Nord, Europe...)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="experience">
+        <h3>Experiences professionnelles</h3>
+        <p>
+          <span>Développeur Front end & Back end</span> stage au sein de la
+          société Marjory de Septembre 2021 à Février 2022
+        </p>
+        <p> paragraphe à compléter</p>
+        <p>
+          <span>
+            Clerc d’Huissier de Justice assermenté et habilité aux constats
+          </span>{' '}
+          au sein de la SCP KINGET-MARLIERE de Septembre 2003 à Mars 2021
+        </p>
+        <p>
+          {' '}
+          Informer la clientèle | Ecouter, dialoguer et négocier avec les
+          parties | Rédiger les courriers et les actes juridiques | Réaliser des
+          constats (dicter et rédiger) | Saisir des écritures comptables |
+          Mettre à jour les actes informatiquement | Analyser et gérer des
+          dossiers | Choisir les procédures | Signifier les actes sur le terrain
+          | Effectuer la veille juridique
+        </p>
+      </div>
+      <div className="formation">
+        <h4>Formation</h4>
+        <p>
+          <span>WILD CODE SCHOOL</span> de Mars 2021 à Juillet 2021
+        </p>
+        <p>
+          {' '}
+          Apprentissage de HTML, CSS, JavaScript, React, NodeJS, ExpressJS,
+          MySQL... mais aussi de Git, Github, la méthode Scrum...
+        </p>
+        <p>
+          {' '}
+          <span>Ecole Nationale de Procédure</span> de Septembre 2003 à Juin
+          2005 (Formation en 2 ans aux procédures civiles d’exécution)
+        </p>
+        <p> Alternance au sein d’une étude d’Huissier de Justice</p>
+        <p>
+          {' '}
+          <span>Faculté Libre de Droit de Lille</span> Obtention de la Maitrise
+          de Droit Privé en 2003
+        </p>
+        <p> Cursus universitaire en droit privé</p>
+      </div>
+    </SCareer>
+  );
 }
--- src/components/Career/Style.jsx
+++ src/components/Career/Style.jsx
@@ -2,6 +2,156 @@ import styled from 'styled-components';
 
 const SCareer = styled.section`
   color: #2c2875;
+  padding-top: 12vh;
+  height: 140vh;
+  h5 {
+    padding-left: 450px;
+    font-size: 3.5em;
+    font-weight: bold;
+    padding-bottom: 50px;
+  }
+  .aboutme {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .image {
+    height: 400px;
+    box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+    border-radius: 5%;
+    img {
+      max-height: 100%;
+      border-radius: 5%;
+    }
+  }
+  .description {
+    .profil {
+      font-size: 1.2em;
+      box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+      border-radius: 5%;
+      width: 65%;
+      padding: 20px;
+      margin: 20px auto;
+    }
+    .softskills {
+      padding-top: 10px;
+      display: flex;
+      justify-content: space-evenly;
+
+      .qualities {
+        box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+        padding: 15px;
+        border-radius: 5%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+        .ecoute {
+          grid-column-start: 1;
+          grid-column-end: 2;
+          grid-row-start: 1;
+          grid-row-end: 2;
+        }
+        .rigueur {
+          grid-column-start: 5;
+          grid-column-end: 6;
+          grid-row-start: 4;
+          grid-row-end: 5;
+        }
+        .autonomie {
+          grid-column-start: 0;
+          grid-column-end: 1;
+          grid-row-start: 3;
+          grid-row-end: 4;
+        }
+        .fiabilite {
+          grid-column-start: 6;
+          grid-column-end: 7;
+          grid-row-start: 5;
+          grid-row-end: 6;
+        }
+        .reflechi {
+          grid-column-start: 1;
+          grid-column-end: 2;
+          grid-row-start: 6;
+          grid-row-end: 7;
+        }
+        .esprit {
+          grid-column-start: 3;
+          grid-column-end: 5;
+          grid-row-start: 1;
+          grid-row-end: 2;
+        }
+        .observateur {
+          grid-column-start: 3;
+          grid-column-end: 5;
+          grid-row-start: 5;
+          grid-row-end: 6;
+        }
+        .analyse {
+          grid-column-start: 5;
+          grid-column-end: 6;
+          grid-row-start: 2;
+          grid-row-end: 3;
+        }
+        h1 {
+          grid-column-start: 2;
+          grid-column-end: 5;
+          grid-row-start: 3;
+          grid-row-end: 4;
+          font-size: 1.3em;
+          font-weight: bold;
+        }
+      }
+      .interest {
+        box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+        padding: 10px;
+        border-radius: 5%;
+        h2 {
+          padding: 10px;
+          text-align: center;
+          font-size: 1.3em;
+          font-weight: bold;
+        }
+        p {
+          margin: 10px;
+        }
+      }
+    }
+  }
+  span {
+    font-weight: bold;
+  }
+  .experience {
+    box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+    padding: 20px;
+    border-radius: 5%;
+    width: 60%;
+    margin: 40px auto;
+    height: 25%;
+    h3 {
+      font-size: 2em;
+      font-weight: bold;
+    }
+    p {
+      padding-top: 20px;
+      font-size: 1.2em;
+    }
+  }
+  .formation {
+    box-shadow: 0.5rem 0.5rem 5rem 0.7rem rgb(0 0 0 / 13%);
+    padding: 20px;
+    border-radius: 5%;
+    width: 60%;
+    margin: 20px auto;
+    height: 21%;
+    h4 {
+      font-size: 2em;
+      font-weight: bold;
+    }
+    p {
+      padding-top: 20px;
+      font-size: 1.2em;
+    }
+  }
 `;
 
 export default SCareer;
